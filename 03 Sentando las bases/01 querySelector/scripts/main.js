11be194a0ec1b69038d384525cea1beda9ed1c57
MatiiAutalan <autalan.matiash@gmail.com>
2020-01-10 16:54:00 -0300

    Ejercicio 1 y 2 no tecnico y ejercicio 1 tecnico Realizados

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,1 +1,9 @@
 console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
+
+const comentarios = document.querySelectorAll ('.comment--text');
+
+comentarios[0].style.backgroundColor = 'green';
+comentarios[0].style.color = 'black';
+comentarios[0].style.fontSize = '20px';
+
+const separadores = document.querySelector('hr');
