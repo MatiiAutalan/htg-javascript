2d7169119515ec68c9d3a3a297e8ba6c91269a7e
MatiiAutalan <autalan.matiash@gmail.com>
2020-01-16 16:01:24 -0300

    Repaso-Ejercicio1-PSMA

--- 03 Sentando las bases/02 addEventListener/scripts/main.js
+++ 03 Sentando las bases/02 addEventListener/scripts/main.js
@@ -2,14 +2,22 @@
 
 const formulario = document.querySelector('#contacto');
 const inputNombre = document.querySelector('#input-nombre');
+const nombreMal = document.querySelector('.nombreIncorrecto')
 
 function recibirNombre(event) {
+  alert ('El Formulario se envio correctamente');
   event.preventDefault();
   console.log(event);
 }
 
 function interceptarNombre(event) {
-  console.log(event.target.value);
+  if (event.target.value.length< 5 || event.target.value.length > 50) {
+    nombreMal.classList.remove ('nombreIncorrecto');
+  } else {
+    nombreMal.classList.add ('nombreIncorrecto');
+  } 
+  
+  console.log(event.target.value.lenght);
 }
 
 formulario.addEventListener('submit', recibirNombre);
